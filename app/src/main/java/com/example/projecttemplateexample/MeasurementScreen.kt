f6f3fa63f1447f4ed0fe724520746b1bf5805cec
juhani.kuru <juhanialeksikuru@gmail.com>
2025-04-12 19:49:36 +0300

    comment MeasurementScreen

--- app/src/main/java/com/example/projecttemplateexample/MeasurementScreen.kt
+++ app/src/main/java/com/example/projecttemplateexample/MeasurementScreen.kt
@@ -44,12 +44,23 @@ import java.time.format.DateTimeFormatter
 
 @Composable
 fun MeasurementScreenRoot(modifier: Modifier = Modifier, vm: MeasurementViewModel) {
+    // measurementState löytyy MeasurementDto-tiedostosta
 
     val measurementState by vm.measurementState.collectAsStateWithLifecycle()
+    // labelState on viewmodelissa tyhjä aluksi, mutta kannattaa pitää viewmodelissa
+    // jos datapisteiden määrä vaihtuu, eikä labelstate päivity datan mukana, ohjelma kaatuu
+    // ongelmaa ei ole, kun labelState on viewmodelissa
     val labelState by vm.labelState.collectAsStateWithLifecycle()
+    // dataformatteria käytetään bottombarissa olevan  napin päivämäärän muotoiluun
+    // se on mallia 12 Apr 2025
     val dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+    // chartin state. Ei kannata laittaa viewmodeliin
+    // kannattaa pitää composablessa, jotta modelProducer kuolee composablen mukana
+    // eikä jää vuotamaan muistia
     val modelProducer = remember { CartesianChartModelProducer() }
 
+    // kun measurementStaten data-attribuutti muuttuu (eli mittausdata haetaan backendista
+    // piirretään chart uudestaan)
     LaunchedEffect(measurementState.data) {
         modelProducer.runTransaction {
             // Learn more: https://patrykandpatrick.com/vmml6t.
@@ -64,13 +75,17 @@ fun MeasurementScreenRoot(modifier: Modifier = Modifier, vm: MeasurementViewMode
     MeasurementScreen(
         measurementState = measurementState,
         dtFormatter = dateFormatter,
+        // onDateBtnClick-callback avaa / sulkee DatePickerin
         onDateBtnClick = {
             vm.setDatePickerVisibility()
+            // onConfirm asettaa valitun päivän ja hakee sille datan
         }, onConfirm = { newDate ->
             vm.setDate(newDate)
         }, modelProducer = modelProducer,
 
         labelState = labelState,
+        // jos tulee virhe datan haussa,
+        // näytetään käyttöliittymäsä virheviesti ja Retry-nappi
         onRetry = {
             vm.getMeasurements()
         }
@@ -95,11 +110,16 @@ fun MeasurementScreen(
             Text("Measurements")
         })
     }, bottomBar = {
+        // bottomappbarissa on nappi, jolla saa datepickerin auki
+
         BottomAppBar {
             TextButton(onClick = {
                 onDateBtnClick()
+                // kun weight(1f) on ainoalla elementillä
+                // bottombarissa, se vie koko leveyden
             }, modifier = Modifier.weight(1f)) {
                 Text(
+                    // näytetään valittu päivämäärä napissa ja keskitetään teksti
                     text = measurementState.date.format(dtFormatter),
                     textAlign = TextAlign.Center
 
@@ -109,6 +129,7 @@ fun MeasurementScreen(
     }) { paddingValues ->
         when {
             measurementState.loading -> {
+                // kun loading on true, näytetään latausikoni
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
@@ -120,6 +141,7 @@ fun MeasurementScreen(
             }
 
             else -> {
+
                 measurementState.error?.let { err ->
                     Box(
                         modifier = Modifier
@@ -127,6 +149,8 @@ fun MeasurementScreen(
                             .padding(paddingValues),
                         contentAlignment = Alignment.Center
                     ) {
+                        // jos tapahtuu virhe, näytetään virhe ja nappi,
+                        // jolla voi hakea datat uudelleen
                         Column(horizontalAlignment = Alignment.CenterHorizontally) {
                             Text(err)
                             Button(onClick = {
@@ -137,32 +161,43 @@ fun MeasurementScreen(
                         }
 
                     }
+                    // jos error on null (eli ei virhettä), piirretään chart
                 } ?: LineChart(
                     measurementState = measurementState,
                     labelState = labelState,
                     modelProducer = modelProducer
                 )
-
+                // kun showDatePicker on true, näytetään DatePicker modaali-ikkunassa
                 if (measurementState.showDatePicker) {
 
+                    // datepickeria käytetään vain täällä, kannatta pitää poissa viewmodelista
                     val datePickerState = rememberDatePickerState(
                         initialSelectedDateMillis = null,
 
                         )
                     DatePickerDialog(
+                        // koska datepicker on modaali-ikkunassa
+                        // kun klikkaat ikkunan ulkopuolelle, se sulkeutuu
                         onDismissRequest = {
                             onDateBtnClick()
                         },
+                        // tämä on Confirm-nappi modaali-ikkunassa
                         confirmButton = {
                             TextButton(
+                                // nappi on aktiivinen vain, jos uusi päivä on valittu
                                 enabled = datePickerState.selectedDateMillis != null,
                                 onClick = {
-
+                                    // valittu päivä on millisekunneissa
+                                    // muutetaan se päivämääräksi
                                     datePickerState.selectedDateMillis?.let { millis ->
                                         val newDate =
                                             Instant.ofEpochMilli(millis)
                                                 .atZone(ZoneId.systemDefault())
                                                 .toLocalDate()
+                                        // kun päivämäärä on valittu
+                                        // ja muutettu oikeaan muotoon
+                                        // kutsutaan funktiota, joka sulkee datepickerin
+                                        // ja hakee datat valitulta päivältä
                                         onConfirm(newDate)
 
                                     }
@@ -171,7 +206,7 @@ fun MeasurementScreen(
                                 }) {
                                 Text("Confirm")
                             }
-
+                        // jos painetaan cance-nappia, suljetaan modaali
                         }, dismissButton = {
                             TextButton(onClick = {
                                 onDateBtnClick()
@@ -209,6 +244,8 @@ fun LineChart(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        // voi olla, että päivältä ei ole dataa saatavilla
+        // näytetään käyttäjälle ilmoitus
         if(measurementState.data.isEmpty()) {
             Text("No data available")
         } else {
